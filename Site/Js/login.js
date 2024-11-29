3dd3669c95af3830fd9313d91b18773ecb798fbd
Turcka <arthur.turcka@gmail.com>
2024-11-29 10:40:22 -0300

    criada função de definição de acesso

--- Site/Js/login.js
+++ Site/Js/login.js
@@ -17,22 +17,30 @@ async function fazerLogin() {
   let passwordLoginInput = document.querySelector('#password')
   let usuarioLogin = usuarioLoginInput.value
   let passwordLogin = passwordLoginInput.value
+  localStorage.setItem("isLoggedIn", false);
   if (usuarioLogin && passwordLogin) {
     try{
-    const loginEndpoint = '/usuarios'
-    const URLcompleta = `${protocolo}${baseURL}${loginEndpoint}`
-    const response = await axios.post(
-      URLcompleta,
-      {user: usuarioLogin, password: passwordLogin}
-    )
-    localStorage.setItem("token", response.data)
-    usuarioLoginInput.value = ""
-    passwordLoginInput.value = ""
-    exibeAlerta('.alert-login', "Usuário logado com sucesso", ['show', 'alert-success'], ['d-none'], 2000)
+      const loginEndpoint = '/usuarios'
+      const URLcompleta = `${protocolo}${baseURL}${loginEndpoint}`
+      const response = await axios.post(
+        URLcompleta,
+        {user: usuarioLogin, password: passwordLogin}
+      )
+      localStorage.setItem("token", response.data)
+      localStorage.setItem("token", response.data.token); 
+      localStorage.setItem("isLoggedIn", true);
+      usuarioLoginInput.value = ""
+      passwordLoginInput.value = ""
+      exibeAlerta('.alert-login', "Usuário logado com sucesso", ['show', 'alert-success'], ['d-none'], 2000)
     }
     catch (e) {
+      localStorage.setItem("isLoggedIn", false);
       exibeAlerta('.alert-modal-login', "Falha na autenticação", ['show', 'alert-danger'], ['d-none'], 2000)
     }
   }
+}
+
+function isUserLoggedIn() {
+  return localStorage.getItem("isLoggedIn") === "true";
 }
 
